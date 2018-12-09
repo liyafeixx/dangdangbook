e52f3a1d847e60ad7029cf7cc5b5084663da99e7
XiaChongYang <2090368817@qq.com>
2018-12-09 15:47:10 +0800

    hhhhhhhhhh

--- src/main/java/com/oracelwpd/ddbookmarket/biz/AdminBiz.java
+++ src/main/java/com/oracelwpd/ddbookmarket/biz/AdminBiz.java
@@ -3,8 +3,6 @@ package com.oracelwpd.ddbookmarket.biz;
 import com.oracelwpd.ddbookmarket.model.Admin;
 
 public interface AdminBiz {
-    //hello
-    //hello
-    //
+
     boolean findAdmin(Admin admin);
 }
