336337c578162c2649da6dab89786b19a07c32ad
XiaChongYang <2090368817@qq.com>
2018-12-09 16:37:06 +0800

    kkkk676776767

--- src/test/java/MianA.java
+++ src/test/java/MianA.java
@@ -1,6 +1,7 @@
 import com.oracelwpd.ddbookmarket.util.MD5Util;
 
 public class MianA {
+    //sss
     public static void main(String[] arge)throws Exception{
         String str="admin";
        System.out.println(MD5Util.getEncryptedPwd(str));
